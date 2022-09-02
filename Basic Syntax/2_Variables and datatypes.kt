a8a412ba0032e65f24a63a900aa18cfd3df912ff
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2022-09-02 21:52:54 +0530

    Create 2_Variables and datatypes.kt

---
+++ Basic Syntax/2_Variables and datatypes.kt
@@ -0,0 +1,16 @@
+fun main() 
+{
+    val foodLike = "Chicken"
+    val veg = "Paratha"
+    println(foodLike)
+    println(veg)
+}
+
+//defining data type
+var num : Int = 100
+num = 500
+
+//print your value within quotes? use $
+println("Number is : ${num}")
+
+//we can reassign value in var
