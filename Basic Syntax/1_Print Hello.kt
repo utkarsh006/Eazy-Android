15b8d084c4c6da864b1e058a7c8b98f11bc72aaf
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2022-09-02 21:41:42 +0530

    Create 1_Print Hello.kt

---
+++ Basic Syntax/1_Print Hello.kt
@@ -0,0 +1,16 @@
+fun main() 
+{
+    print("Utkarsh")
+    print("Saxena")
+}
+
+//Output : UtkarshSaxena
+
+fun main() 
+{
+    println("Utkarsh")
+    println("Saxena")
+}
+
+/* Output : Utkarsh
+            Saxena  */
