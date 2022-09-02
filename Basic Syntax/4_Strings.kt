f7e785ef798ce290fa927b4ecbe83091ff32c65f
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2022-09-02 22:26:24 +0530

    Create 4_Strings.kt

---
+++ Basic Syntax/4_Strings.kt
@@ -0,0 +1,10 @@
+fun main() 
+{  
+    var myString = "Utkarsh Saxena"
+    println(myString)
+    println(myString.length)
+    println(myString.get(5))
+
+    var addStr = " is here"
+    println(myString.plus(addStr))
+}
